72db486e39e79bc6796ce96b7a70b5a39722b5c5
Angela Appiah <alegnalawn2@yahoo.com>
2020-02-06 18:25:55 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,8 +4,8 @@ def my_hash(hash)
 end
 
 def 
-  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil painting" => 3}
-  puts the_manifest ["oil painting"]
+  the_manifest = {}
+ puts the_manifest ["oil painting"]
 end 
 
 
